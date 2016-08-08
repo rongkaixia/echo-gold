bf3c2c2ffcb060348f61ee7409a6e6b838423312
rongkaixia <rongkaixia@gmail.com>
2016-08-08 12:13:15 +0800

    add slf4j and log4j

--- src/main/scala/com/echo/gold/server.scala
+++ src/main/scala/com/echo/gold/server.scala
@@ -2,12 +2,20 @@ package com.echo.gold
 
 import java.util.logging.Logger
 
+import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
+
 import io.grpc.{Server, ServerBuilder}
 import scala.concurrent.{ExecutionContext, Future}
 import com.typesafe.config.ConfigFactory
+import com.typesafe.scalalogging.LazyLogging
 import com.echo.gold.protocol._
 
-import org.mongodb.scala.MongoClient
+import org.mongodb.scala.{MongoClient, 
+                          MongoDatabase,
+                          MongoClientSettings, 
+                          ServerAddress}
+import com.mongodb.connection._
 
 /**
  * [[https://github.com/grpc/grpc-java/blob/v0.13.2/examples/src/main/java/io/grpc/examples/helloworld/OrderServer.java]]
@@ -22,15 +30,19 @@ object OrderServer {
   private val port = 50051
 }
 
-class OrderServer(executionContext: ExecutionContext) { self =>
+class OrderServer(executionContext: ExecutionContext) extends LazyLogging{ self =>
   private[this] var server: Server = null
   val cfg = ConfigFactory.load()
-  val log = Logger.getLogger(classOf[OrderServer].getName)
-  val mongoClient: MongoClient = MongoClient("mongodb://localhost")
+  System.setProperty("log4j.configuration", "log4j.xml");
 
   private def start(): Unit = {
-    server = ServerBuilder.forPort(OrderServer.port).addService(OrderServiceGrpc.bindService(new OrderImpl, executionContext)).build.start
-    log.info("Server started, listening on " + OrderServer.port)
+    // init mongodb
+    implicit val mongoClient: MongoClient = initMongo()
+    server = ServerBuilder.forPort(OrderServer.port)
+                          .addService(OrderServiceGrpc.bindService(new OrderImpl, executionContext))
+                          .build
+                          .start
+    logger.info("Server started, listening on " + OrderServer.port)
     Runtime.getRuntime.addShutdownHook(new Thread() {
       override def run(): Unit = {
         System.err.println("*** shutting down gRPC server since JVM is shutting down")
@@ -52,9 +64,63 @@ class OrderServer(executionContext: ExecutionContext) { self =>
     }
   }
 
-  private class OrderImpl extends OrderServiceGrpc.OrderService {
+  private def initMongo(): MongoClient = {
+    val host = cfg.getString("echo.gold.mongo.host")
+    val port = cfg.getInt("echo.gold.mongo.port")
+    logger.info(s"mongodb[host=${host}, port=${port}")
+    val clusterSettings: ClusterSettings = 
+      ClusterSettings.builder().hosts(List(new ServerAddress(host, port)).asJava).build()
+    val settings: MongoClientSettings = 
+      MongoClientSettings.builder().clusterSettings(clusterSettings).build()
+    MongoClient(settings)
+  }
+
+  private class OrderImpl()(implicit mongo: MongoClient) 
+    extends OrderServiceGrpc.OrderService with LazyLogging {
+
+    private def pricing(req: OrderRequest): OrderInfo = {
+      val price = 1000.00
+      val realPrice = 1000.00
+      val discount = 0.0
+      val payAmt = realPrice * req.num
+      val realPayAmt = payAmt + discount
+
+      OrderInfo(userId = req.userId,
+                title = req.title,
+                productId = req.productId,
+                num = req.num,
+                payMethod = req.payMethod,
+                deliverMethod = req.deliverMethod,
+                recipientsName = req.recipientsName,
+                recipientsPhone = req.recipientsPhone,
+                recipientsAddress = req.recipientsAddress,
+                recipientsPostcode = req.recipientsPostcode,
+                comment = req.comment,
+                price = price,
+                realPrice = realPrice,
+                discount = discount,
+                payAmt = payAmt,
+                realPayAmt = realPayAmt)
+    }
+
+    private def saveToMongo(): Unit = {
+      val database: MongoDatabase = mongo.getDatabase("mydb")
+      logger.debug(s"mongo database: ${database}")
+    }
+
     override def order(req: OrderRequest) = {
-      log.info(s"recieve request: ${req}")
+      logger.debug(s"recieve request: ${req}")
+      // check request
+      
+      // generate order id
+      val id = 
+      // pricing
+      val orderInfo = pricing(req)
+
+      // write to db
+      saveToMongo(orderInfo)
+
+      // send response
       val header = ResponseHeader(ResultCode.SUCCESS, "")
       val reply = OrderResponse().withHeader(header)
       Future.successful(reply)
